d7dd0f5afd2bc4a2cf34db260b305d07ab826167
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-27 16:44:44 +0300

    Add functionality to add a name

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+// Elements
+const okBtnElement = document.getElementById('okBtn');
+const nameElement = document.getElementById('nameInput');
+const nameDivElement = document.querySelector('.name');
+const addTaskElement = document.querySelector('.addTask');
+const listElement = document.querySelector('.list');
+const spanNameElement = document.getElementById('nameSpan');
+let name = ''
 
-const okBtnElement = document.getElementsById('okBtn');
-const nameElement = document.getElementById('nameInput')
+// Events
 
-
-
-okBtnElement.addEventListener('click', () => {
-
-    form.addEventListener('click', (e) => {
-        e.preventDefault();
-    })
-    const name = nameElement.ariaValueMax;
-    console.log(name);
+okBtnElement.addEventListener('click', (e) => {
+    name = nameElement.value;
+    nameElement.value = '';
+    spanNameElement.textContent = name;
+    nameDivElement.style.display = 'none';
+    addTaskElement.classList.remove('hidden');
+    listElement.classList.remove('hidden');
 })
