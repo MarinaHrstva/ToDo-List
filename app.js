2214ff10978b3f4b25e9ef88fd4f55178707073f
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-27 16:29:59 +0300

    Add styles

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('it works');
+
+const okBtnElement = document.getElementsById('okBtn');
+const nameElement = document.getElementById('nameInput')
+
+
+
+okBtnElement.addEventListener('click', () => {
+
+    form.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+    const name = nameElement.ariaValueMax;
+    console.log(name);
+})
