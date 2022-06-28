75f74c54d1be6ba63d8212c83028a92c44e3ca4a
Marina Hristova <marina.ts.hristova@gmail.com>
2022-06-28 11:53:27 +0300

    Add done functionality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const addTaskInputElement = document.querySelector('.addTask input')
 
 const addTaskBtnElement = document.querySelector('#addTaskBtn');
 
+const container = document.querySelector('.container')
 const spanNameElement = document.getElementById('nameSpan');
 const listElement = document.querySelector('.list ul');
 
@@ -14,35 +15,65 @@ let name = ''
 
 // Events
 
-// okBtnElement.addEventListener('click', (e) => {
-//     name = nameElement.value;
-//     nameElement.value = '';
-//     spanNameElement.textContent = name;
-//     nameDivElement.style.display = 'none';
-//     addTaskElement.classList.remove('hidden');
-//     listElement.classList.remove('hidden');
-// })
+okBtnElement.addEventListener('click', start)
 
 addTaskBtnElement.addEventListener('click', (e) => {
     const task = addTaskInputElement.value;
+
+    if (!task) {
+        return;
+    }
+
     const liElement = createTaskLi(task);
     listElement.appendChild(liElement);
+    addTaskInputElement.value = '';
 })
 
+listElement.addEventListener('click', done)
+
+
+
+
+
+
+
+// Functions
+
+function start(e) {
+    name = nameElement.value;
+    nameElement.value = '';
+    spanNameElement.textContent = name;
+    nameDivElement.style.display = 'none';
+    addTaskElement.classList.remove('hidden');
+    listElement.classList.remove('hidden');
+    container.style.display = 'flex'
+}
+
+
+function done(e) {
+    const button = e.target
+    if (button.textContent == 'Delete') {
+        button.parentNode.parentNode.classList.toggle('delete');
+    } else if (e.target.textContent == 'Done') {
+        button.parentNode.parentNode.classList.toggle('done');
+    }
+}
 
 function createTaskLi(task) {
     const li = document.createElement('li');
+
     const doneBtn = document.createElement('button');
     doneBtn.textContent = 'Done';
-    doneBtn.classList.add('done');
     const deleteBtn = document.createElement('button')
     deleteBtn.textContent = 'Delete'
-    doneBtn.classList.add('delete');
 
+    const btnsContainer = document.createElement('div');
+    btnsContainer.classList.add('btnsContainer');
+    btnsContainer.appendChild(doneBtn);
+    btnsContainer.appendChild(deleteBtn);
 
     li.textContent = task;
-    li.appendChild(doneBtn);
-    li.appendChild(deleteBtn);
-
+    li.appendChild(btnsContainer);
     return li;
 }
+
